2da42a1cfd92f7cd1d4c67589e670853bf88c18e
thurdas <havlisarts@gmail.com>
2017-09-09 16:18:48 +0200

    login
    login

--- index.php
+++ index.php
@@ -18,19 +18,30 @@
     <link rel="stylesheet" type="text/css" href="./css/reset.css">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet">
 </head>
 <body>
-<form id="login-form" action="">
-    <div class="container">
-        <h1>Freelancer</h1>
-        <input type="text" placeholder="Enter Username" name="username" required>
-        <input type="password" placeholder="Enter Password" name="password" required>
-        <button id="submit-button" type="submit">Login</button>
-        <button id="loading-button">
-            <i class="fa fa-circle-o-notch fa-spin"></i>Loading
-        </button>
-    </div>
-</form>
+<section class="bg"></section>
+<section id="login-page">
+    <article>
+        <form class="login-form" action="">
+            <div class="container">
+                <h1>Kafy</h1>
+                <section class="form-wrap">
+                <input type="text" placeholder="Username" name="username" required>
+                <input type="password" placeholder="Password" name="password" required>
+                <button id="submit-button" type="submit">Login</button>
+                <button id="loading-button">
+                    <i class="fa fa-circle-o-notch fa-spin"></i>Loading
+                </button>
+                    <section class="register-login">Dont have account ? <a href="#">Register here!</a></section>
+                </section>
+
+            </div>
+        </form>
+    </article>
+</section>
+
 <script type="text/javascript">
     //Replaces login button with loading button on submit
     window.addEventListener("submit", function(event){
